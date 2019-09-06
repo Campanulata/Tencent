72e0a1a83dcd1dc572431183578e67cd9f778e20
campanulata <ts1989512@outlook.com>
2019-09-06 18:20:42 +0800

    增加数据分析功能

---
+++ data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pandas_profiling
+import os
+
+data = pd.read_excel('1.xlsx')
+# data.profile_report('Titanic Dataset')
+pandas_profiling.ProfileReport(data)
+profile = data.profile_report(title="oil_data")
+profile.to_file(output_file="oil_data.html")
