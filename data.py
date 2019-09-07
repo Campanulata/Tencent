51726b1d0e140624e636ed414dafdc8900e0215f
campanulata <ts1989512@outlook.com>
2019-09-07 14:22:28 +0800

    删除旧数据

--- data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pandas_profiling
-import os
-
-data = pd.read_excel('test1.xlsx')
-# data.profile_report('Titanic Dataset')
-pandas_profiling.ProfileReport(data)
-profile = data.profile_report(title="学习情况数据分析")
-profile.to_file(output_file="学习情况数据分析.html")
